6c51739547cffde088ed4616df370568053cf59d
muirujackson <jakezweru@gmail.com>
2023-05-03 23:22:49 -0400

    Image Slider

--- js/main.js
+++ js/main.js
@@ -4,4 +4,15 @@ $(document).ready( function(){
         play: 4000,
         pagination:false
     })
+
+    var typed = new Typed('.typed', {
+        strings: ['<i>Software</i> Engineer', '&amp; Web Developer.'],
+        typeSpeed: 70,
+        backSpeed: 20,
+        backDelay: 500,
+        startDelay: 1000,
+        loop: true,
+        cursorChar: '_',
+      });
 });
+
